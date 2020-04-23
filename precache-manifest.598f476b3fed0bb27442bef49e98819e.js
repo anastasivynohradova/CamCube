97ba158b4ace0a6f4adf3176b865cd5ce8fe2e08
ihor-doroschenko <igordoroshenko@outlook.com>
2020-04-23 18:19:47 +0200

    Updates

---
+++ precache-manifest.598f476b3fed0bb27442bef49e98819e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8857f32f7de2ff0ceeda15794cc5e2e9",
+    "url": "/cam-cube/index.html"
+  },
+  {
+    "revision": "675cc3adb961d613718e",
+    "url": "/cam-cube/static/css/main.f52dfacc.chunk.css"
+  },
+  {
+    "revision": "ab5cf3967864ec0eba2b",
+    "url": "/cam-cube/static/js/2.ef2e7f45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cam-cube/static/js/2.ef2e7f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675cc3adb961d613718e",
+    "url": "/cam-cube/static/js/main.b844c628.chunk.js"
+  },
+  {
+    "revision": "a8936244ee59e7eb6b9f",
+    "url": "/cam-cube/static/js/runtime-main.f57e8781.js"
+  },
+  {
+    "revision": "52f76c0c34664e8e8d61c0017486a0a7",
+    "url": "/cam-cube/static/media/AERONET.52f76c0c.png"
+  },
+  {
+    "revision": "1fbbebd61a48912c81acc4f0e6410baa",
+    "url": "/cam-cube/static/media/CamCubeMain.1fbbebd6.png"
+  },
+  {
+    "revision": "2be5e21bf77fbac1a2c7ccacaaa6dd94",
+    "url": "/cam-cube/static/media/Polarimeter.2be5e21b.png"
+  },
+  {
+    "revision": "d4dda8afb8ebe85f5bd2a599b15599aa",
+    "url": "/cam-cube/static/media/Rakete.d4dda8af.gif"
+  },
+  {
+    "revision": "06d574f599bf69647c684b971e826961",
+    "url": "/cam-cube/static/media/ferntransport.06d574f5.jpg"
+  },
+  {
+    "revision": "6b0e768a73a9cd624302398a2753703f",
+    "url": "/cam-cube/static/media/fifth.6b0e768a.jpg"
+  },
+  {
+    "revision": "36b7cd89d6cfbfc8de6eb8a09cfe35bd",
+    "url": "/cam-cube/static/media/fourth.36b7cd89.jpg"
+  },
+  {
+    "revision": "d876a05e1898d46076de673f1fbb2e32",
+    "url": "/cam-cube/static/media/modern_no.20-webfont.d876a05e.woff"
+  },
+  {
+    "revision": "4df56219a1694922f606768db58d4f83",
+    "url": "/cam-cube/static/media/second.4df56219.png"
+  },
+  {
+    "revision": "a756497db1d5009d9c4d768dc37987c4",
+    "url": "/cam-cube/static/media/sixth.a756497d.jpg"
+  },
+  {
+    "revision": "95715f5510cb481170d954fa2795c2e3",
+    "url": "/cam-cube/static/media/unilogo.95715f55.jpg"
+  }
+]);
